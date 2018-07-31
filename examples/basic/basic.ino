972566c6fc5777a3c75ca9fc665ca5c8c58431ee
Jan Jurca <jjurca@redhat.com>
2018-07-31 12:11:38 +0200

    Example fix

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -6,10 +6,13 @@ Highprom prom(100); //Reserve first 100 bytes to be used by this library
 void setup(){
     Serial.begin(9600);
 
-    while (!Serial.available()) {
-       if (Serial.read() == 'e') {
-           prom.init(); // This erases eeprom and prepare it to be used ba lib, It is not need to run it every time, just when you start to use this
-       }
+    while (1) {
+        if (Serial.available()) {
+            if (Serial.read() == 'e') {
+                prom.init(); // This erases eeprom and prepare it to be used ba lib, It is not need to run it every time, just when you start to use this
+                break;
+            }
+        }
        if (millis() > 10000) {
            //user doesnt erased it in first 10 seconds so go on to program
            break;
